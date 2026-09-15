96e509b4fa9710303b17377ad364c31df000f039
Muhammad Saqib <mughalsaqib815@gmail.com>
2026-09-15 14:28:20 +0500

    fix: update cache-control header for sitemap.xml to allow immediate revalidation

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -39,7 +39,7 @@ ${pages
     status: 200,
     headers: {
       'Content-Type': 'application/xml; charset=UTF-8',
-      'Cache-Control': 'public, max-age=86400, s-maxage=86400, stale-while-revalidate=86400',
+      'Cache-Control': 'public, max-age=0, must-revalidate',
     },
   });
 }
