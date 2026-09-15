4dd26dfe529aac429a26976b51d512a3f283f38a
Muhammad Saqib <mughalsaqib815@gmail.com>
2026-09-15 12:53:11 +0500

    update for search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: 'VideoSave - Free Online Video Downloader (4K, 1080p, MP4, MP3)',
   description: 'Download TikTok videos without watermark, Instagram Reels, Facebook videos, X (Twitter), Vimeo, and Pinterest videos in HD & 4K for free. Fast, unlimited, online video saver.',
   verification: {
-    google: 'googleea928f32fd8215bc'
+    google: 'Fwmfc4CpcN7fHZBnUDkKP7NhL1bJ8LpWe-vt8Ry0Vyc'
   },
   keywords: [
     'videosave',
